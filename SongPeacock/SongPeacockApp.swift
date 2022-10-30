a11a747992b6f968b963bd0fc2a5c2d083d98fd6
Morten Bjerg Gregersen <morten@mogee.dk>
2022-10-30 21:00:16 +0100

    Initial Commit

---
+++ SongPeacock/SongPeacockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongPeacockApp.swift
+//  SongPeacock
+//
+//  Created by Morten Bjerg Gregersen on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SongPeacockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
